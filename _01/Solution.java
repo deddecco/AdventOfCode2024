0256389de44ea043e6f72f22c88c9fd1ac6024c6
Andre da Silva <apdasil@emory.edu>
2024-12-01 14:56:50 -0500

    passing in the full data set (no longer just easy-data), this produces the correct answer

--- _01/Solution.java
+++ _01/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution {
@@ -13,8 +14,11 @@ public class Solution {
      public static void main(String[] args) throws IOException {
           Solution solution = new Solution();
           solution.readFile(args[0]);
-          System.out.println(leftList);
-          System.out.println(rightList);
+          Collections.sort(leftList);
+          Collections.sort(rightList);
+
+          int result = solution.calculateDistance(leftList, rightList);
+          System.out.println("total distance: " + result);
      }
 
      private void readFile(String fileName) throws IOException {
@@ -26,6 +30,16 @@ public class Solution {
                leftList.add(Integer.parseInt(arr[0]));
                rightList.add(Integer.parseInt(arr[1]));
           }
+     }
+
+     public int calculateDistance(List<Integer> leftList, List<Integer> rightList) {
+          int totalDistance = 0;
+
+          for (int i = 0; i < leftList.size(); i++) {
+               totalDistance += Math.abs(leftList.get(i) - rightList.get(i));
+          }
+
+          return totalDistance;
      }
 
 

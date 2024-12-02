dad0fb9bcb5beb6a9dd06ea47c281defeac67984
Andre da Silva <apdasil@emory.edu>
2024-12-02 10:09:01 -0500

    start working on day 2

--- _02/Solution.java
+++ _02/Solution.java
@@ -1,4 +1,40 @@
 package _02;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Solution {
+
+     int countSafe = 0;
+
+     private int[][] transformFile(String fileName) throws IOException {
+          BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+          String line;
+          int[][] file = new int[6][];
+          int fileIdx = 0;
+          while ((line = reader.readLine()) != null) {
+               String[] arr = line.split("\t");
+               int[] lineArray = new int[arr.length];
+               for (int i = 0; i < arr.length; i++) {
+                    lineArray[i] = Integer.parseInt(arr[i]);
+               }
+               file[fileIdx] = lineArray;
+          }
+
+          return file;
+     }
+
+     public boolean safe(int[] level) {
+          for (int i = 1; i < level.length - 1; i++) {
+               if (!(Math.abs(level[i] - level[i - 1]) >= 1 && Math.abs(level[i] - level[i - 1]) <= 3)) {
+                    return false;
+               }
+          }
+          countSafe++;
+          return true;
+     }
+
+
 }
